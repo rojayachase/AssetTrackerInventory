b63dedc444d53e6c50b8dcff7510ba8ac42c73be
rojayachase <rojayachase@gmail.com>
2023-11-16 21:12:15 -0500

    fetch tested

--- formSubmit.js
+++ formSubmit.js
@@ -4,6 +4,15 @@ form.addEventListener('submit', function (e) {
         e.preventDefault();
 
         const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+
+        fetch('https://reqres.in/api/users', {
+            method: 'POST' ,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
 
         console.log([...formData]);
 
